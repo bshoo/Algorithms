3924858061edf28be73db55946f81c736c61437d
bshoo <69185105+bshoo@users.noreply.github.com>
2024-02-28 12:23:50 +0530

    Update bfs.py

--- Code/bfs.py
+++ Code/bfs.py
@@ -1,1 +1,21 @@
-nodes = {0:[1, 3],1:[0,3,5],2:[4, 6],3:[1, 0, 5, 7],4:[7, 6, 2],5:[1, 3, 6],6:[2, 4, 5, 7],7:[4, 6, 3]}visited = []queue = []def BFS():for node in nodes:if node not in visited:bfs(node)def bfs(n):visited.append(n)queue.append(n)while len(queue)!=0:for w in sorted(nodes[queue[0]]):if w not in visited:visited.append(w)queue.append(w)del queue[0]BFS()print("BFS: ",visited)
+nodes = {0:[1, 3],1:[0,3,5],2:[4, 6],3:[1, 0, 5, 7],4:[7, 6, 2],5:[1, 3, 6],6:[2, 4, 5, 7],7:[4, 6, 3]}
+visited = []
+queue = []
+
+def BFS():
+  for node in nodes:
+    if node not in visited:
+      bfs(node)
+
+def bfs(n):
+  visited.append(n)
+  queue.append(n)
+  while len(queue)!=0:
+    for w in sorted(nodes[queue[0]]):
+      if w not in visited:
+        visited.append(w)
+        queue.append(w)
+      del queue[0]
+
+BFS()
+print("BFS: ",visited)
